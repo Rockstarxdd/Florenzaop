29fa52cc0d847abe1e9805f5b34abc8e5e364fbf
ROCKSTAR OP <88956892+T5FR0CK5T4R@users.noreply.github.com>
2021-12-15 19:42:37 +0530

    Update alive.py

--- MashaRoBot/modules/alive.py
+++ MashaRoBot/modules/alive.py
@@ -10,7 +10,7 @@ async def awake(event):
   LOVELY = "**𝗪𝗛𝗢 𝗔𝗠 𝗜 🥺❓** \n\n"
   LOVELY += "**I'ᴍ 𝙵𝚕𝚘𝚛𝚎𝚗𝚣𝚊💞, Yᴏᴜʀ Hᴇᴀʀᴛʙᴇᴀᴛ❤️🙈*\n\n"
   LOVELY += "**I'ᴍ Wᴏʀᴋɪɴɢ Oɴ Lᴏᴠᴇ 😜**\n\n"
-  LOVELY += "**Mʏ σωηєя :**  [🖤 𝗦𝗧𝗔𝗥𝗚𝗜𝗥𝗟🖤🇮🇳™](t.me/TUSHAR204)\n\n"
-  LOVELY += "**Aʙᴏᴜᴛ Mʏ 𝙾𝚠𝚗𝚎𝚛 🤩 :** [🖤 𝗦𝗧𝗔𝗥𝗚𝗜𝗥𝗟🖤🇮🇳™](t.me/ABOUTVEDMAT)\n\n"
+  LOVELY += "**Mʏ σωηєя :**  [🖤 𝗦𝗧𝗔𝗥𝗚𝗜𝗥𝗟🖤🇮🇳™](t.me/STA4RGIRL_XD)\n\n"
+  LOVELY += "**Aʙᴏᴜᴛ Mʏ 𝙾𝚠𝚗𝚎𝚛 🤩 :** [🖤 𝗦𝗧𝗔𝗥𝗚𝗜𝗥𝗟🖤🇮🇳™](t.me/itzmeehh)\n\n"
   BUTTON = [[Button.url("𝗦𝗨𝗣𝗣𝗢𝗥𝗧🙂", "https://t.me/FlorenzaSupport"), Button.url("𝗨𝗣𝗗𝗔𝗧𝗘", "https://t.me/Starz_bots")]]
   await love.send_file(event.chat_id, PHOTO, caption=LOVELY,  buttons=BUTTON)
