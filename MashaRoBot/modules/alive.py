06b095755aeb04c2357433a13e760cea6aa1e8e2
ROCKSTAR OP <88956892+T5FR0CK5T4R@users.noreply.github.com>
2021-12-15 19:57:42 +0530

    Update alive.py

--- MashaRoBot/modules/alive.py
+++ MashaRoBot/modules/alive.py
@@ -2,15 +2,14 @@ from telethon import events, Button, custom
 import re, os
 from MashaRoBot.events import register
 from MashaRoBot import telethn as love
-from MashaRoBot import telethn as tgbot
 PHOTO = "https://telegra.ph/file/ec7c7881de4fca0bb7484.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   LOVELY = event.sender.first_name
   LOVELY = "**𝗪𝗛𝗢 𝗔𝗠 𝗜 🥺❓** \n\n"
-  LOVELY += "**I'ᴍ 𝙵𝚕𝚘𝚛𝚎𝚗𝚣𝚊💞, Yᴏᴜʀ Hᴇᴀʀᴛʙᴇᴀᴛ❤️🙈*\n\n"
-  LOVELY += "**I'ᴍ Wᴏʀᴋɪɴɢ Oɴ Lᴏᴠᴇ 😜**\n\n"
+  LOVELY += "**I'ᴍ 𝙵𝚕𝚘𝚛𝚎𝚗𝚣𝚊🥰, Yᴏᴜʀ Hᴇᴀʀᴛʙᴇᴀᴛ❤️🔥*\n\n"
+  LOVELY += "**I'ᴍ Wᴏʀᴋɪɴɢ Oɴ Lᴏᴠᴇ 🦋**\n\n"
   LOVELY += "**Mʏ σωηєя :**  [🖤 𝗦𝗧𝗔𝗥𝗚𝗜𝗥𝗟🖤🇮🇳™](t.me/STA4RGIRL_XD)\n\n"
   LOVELY += "**Aʙᴏᴜᴛ Mʏ 𝙾𝚠𝚗𝚎𝚛 🤩 :** [🖤 𝗦𝗧𝗔𝗥𝗚𝗜𝗥𝗟🖤🇮🇳™](t.me/itzmeehh)\n\n"
   BUTTON = [[Button.url("𝗦𝗨𝗣𝗣𝗢𝗥𝗧🙂", "https://t.me/FlorenzaSupport"), Button.url("𝗨𝗣𝗗𝗔𝗧𝗘", "https://t.me/Starz_bots")]]
-  await love.send_file(event.chat_id, PHOTO, caption=LOVELY,  buttons=BUTTON)
+  await love.send_file(event.chat_id, PHOTO, caption=FLORENZA,  buttons=BUTTON)
